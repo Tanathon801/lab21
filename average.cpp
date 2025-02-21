cfdce1a9388cbf38efa43112f9990ae67e9707c1
Tanathon801 <aumatomic@gmail.com>
2025-02-21 17:29:45 +0700

    Update average.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+using namespace std;
+
+int main(int n, char *s[]){
+
+    double sum = 0.0;
+    double avg;
+
+    if(n == 1){
+        cout << "Please input numbers to find average.";
+    }
+    else{
+        cout << "---------------------------------" << endl;
+        for(int i = 0; i < n; i++){
+            sum += atof(s[i]);
+        }
+
+        avg = sum / (n - 1);
+        cout << "Average of "<< n-1 << " numbers = " << avg << endl;
+        cout << "---------------------------------" << endl;
+    }
+    return 0;
+}
